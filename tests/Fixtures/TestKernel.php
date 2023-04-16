bec0dd54e729c3a60e303b99af4496d1229ea3c4
Burak Bolat <burak.bolat@hprotravel.com>
2023-04-17 00:39:15 +0300

    Replace symfony/phpunit-bridge with phpunit/phpunit

--- tests/Fixtures/TestKernel.php
+++ tests/Fixtures/TestKernel.php
@@ -2,32 +2,35 @@
 
 namespace Goksagun\SchedulerBundle\Tests\Fixtures;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Goksagun\SchedulerBundle\Tests\Fixtures\FooBundle\FooBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
 class TestKernel extends Kernel
 {
-    public function registerBundles()
+    public function registerBundles(): array
     {
-        return array(
-            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new \Goksagun\SchedulerBundle\Tests\Fixtures\FooBundle\FooBundle(),
-        );
+        return [
+            new FrameworkBundle(),
+            new DoctrineBundle(),
+            new FooBundle(),
+        ];
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $loader->load(__DIR__.'/config/config.yml');
+        $loader->load(__DIR__ . '/config/config.yml');
 
         if (PHP_VERSION_ID >= 70100) {
-            $loader->load(__DIR__.'/config/nullable_type/config.yml');
+            $loader->load(__DIR__ . '/config/nullable_type/config.yml');
         }
     }
 
-    public function getCacheDir()
+    public function getCacheDir(): string
     {
-        return __DIR__.'/cache/'.$this->environment;
+        return __DIR__ . '/cache/' . $this->environment;
     }
 }
 
