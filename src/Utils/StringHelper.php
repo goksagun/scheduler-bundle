6d3a00d93b84ed7fd22d74d6ce60398887c229ba
Burak Bolat <burak.bolat@hprotravel.com>
2023-04-18 00:44:53 +0300

    Add StringHelperTest class

--- src/Utils/StringHelper.php
+++ src/Utils/StringHelper.php
@@ -4,17 +4,17 @@ namespace Goksagun\SchedulerBundle\Utils;
 
 final class StringHelper
 {
+    private function __construct()
+    {
+    }
+
     /**
      * Determine if a given string starts with a given substring.
-     *
-     * @param string $haystack
-     * @param string|array $needles
-     * @return bool
      */
-    public static function startsWith($haystack, $needles)
+    public static function startsWith(string $haystack, string|array $needles): bool
     {
         foreach ((array)$needles as $needle) {
-            if ($needle !== '' && substr($haystack, 0, strlen($needle)) === (string)$needle) {
+            if (str_starts_with($haystack, $needle)) {
                 return true;
             }
         }
@@ -24,15 +24,11 @@ final class StringHelper
 
     /**
      * Determine if a given string ends with a given substring.
-     *
-     * @param string $haystack
-     * @param string|array $needles
-     * @return bool
      */
-    public static function endsWith($haystack, $needles)
+    public static function endsWith(string $haystack, array|string $needles): bool
     {
         foreach ((array)$needles as $needle) {
-            if (substr($haystack, -strlen($needle)) === (string)$needle) {
+            if (str_ends_with($haystack, $needle)) {
                 return true;
             }
         }
@@ -42,15 +38,11 @@ final class StringHelper
 
     /**
      * Determine if a given string contains a given substring.
-     *
-     * @param string $haystack
-     * @param string|array $needles
-     * @return bool
      */
-    public static function contains($haystack, $needles)
+    public static function contains(string $haystack, array|string $needles): bool
     {
         foreach ((array)$needles as $needle) {
-            if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
+            if (str_contains($haystack, $needle)) {
                 return true;
             }
         }
@@ -60,18 +52,13 @@ final class StringHelper
 
     /**
      * Limit the number of characters in a string.
-     *
-     * @param string $value
-     * @param int $limit
-     * @param string $end
-     * @return string
      */
-    public static function limit($value, $limit = 100, $end = '...')
+    public static function limit(string $value, int $limit = 100, string $end = '...'): string
     {
         if (mb_strwidth($value, 'UTF-8') <= $limit) {
             return $value;
         }
 
-        return rtrim(mb_strimwidth($value, 0, $limit, '', 'UTF-8')).$end;
+        return rtrim(mb_strimwidth($value, 0, $limit, '', 'UTF-8')) . $end;
     }
 }
