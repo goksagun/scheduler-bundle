8d289dff78759dfce8c5cc341132d315a877b8c0
Burak Bolat <burak.bolat@hprotravel.com>
2023-04-18 01:01:39 +0300

    Fix namespace

--- tests/Controller/SchedulerControllerTest.php
+++ tests/Controller/SchedulerControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Controller;
+namespace Goksagun\SchedulerBundle\Tests\Controller;
 
 use PHPUnit\Framework\TestCase;
 
--- tests/Utils/StringHelperTest.php
+++ tests/Utils/StringHelperTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Utils;
+namespace Goksagun\SchedulerBundle\Tests\Utils;
 
 use Goksagun\SchedulerBundle\Utils\StringHelper;
 use PHPUnit\Framework\TestCase;
