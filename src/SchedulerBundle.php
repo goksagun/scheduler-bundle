f0c9af6b78ae41b31dd4645100da32daad9b25d6
Burak Bolat <burak.bolat@hprotravel.com>
2023-04-17 09:37:22 +0300

    Override SchedulerBundle:getPath method for compatibility new bundle structure

--- src/SchedulerBundle.php
+++ src/SchedulerBundle.php
@@ -6,5 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SchedulerBundle extends Bundle
 {
-
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
