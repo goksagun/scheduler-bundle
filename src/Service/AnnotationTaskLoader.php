8bc0b9d4ff90f467aa7e573d3b140d5c1a9119b5
Burak Bolat <burak.bolat@hprotravel.com>
2023-04-28 01:40:07 +0300

    Refactor: remove unnecessary if statement

--- src/Service/AnnotationTaskLoader.php
+++ src/Service/AnnotationTaskLoader.php
@@ -47,53 +47,51 @@ class AnnotationTaskLoader extends AbstractTaskLoader implements TaskLoaderInter
             }
 
             foreach ($annotations as $annotation) {
-                if ($annotation instanceof Schedule) {
-                    $annotationTask = $annotation->toArray();
+                $annotationTask = $annotation->toArray();
 
-                    $task = [];
-                    foreach (AttributeInterface::ATTRIBUTES as $attribute) {
-                        if (AttributeInterface::ATTRIBUTE_ID == $attribute) {
-                            // Generate Id.
-                            $id = HashHelper::generateIdFromProps(
-                                ArrayUtils::only($annotationTask, HashHelper::GENERATED_PROPS)
-                            );
+                $task = [];
+                foreach (AttributeInterface::ATTRIBUTES as $attribute) {
+                    if (AttributeInterface::ATTRIBUTE_ID == $attribute) {
+                        // Generate Id.
+                        $id = HashHelper::generateIdFromProps(
+                            ArrayUtils::only($annotationTask, HashHelper::GENERATED_PROPS)
+                        );
 
-                            $task[$attribute] = $id;
+                        $task[$attribute] = $id;
 
-                            continue;
-                        }
-
-                        if (AttributeInterface::ATTRIBUTE_STATUS == $attribute) {
-                            if (!isset($annotationTask[$attribute])) {
-                                $task[$attribute] = StatusInterface::STATUS_ACTIVE;
-                            } else {
-                                $task[$attribute] = $annotationTask[$attribute];
-                            }
-
-                            continue;
-                        }
-
-                        if (AttributeInterface::ATTRIBUTE_RESOURCE == $attribute) {
-                            $task[$attribute] = ResourceInterface::RESOURCE_ANNOTATION;
-
-                            continue;
-                        }
-
-                        $task[$attribute] = $annotationTask[$attribute] ?? null;
-                    }
-
-                    // Filter by status
-                    if (null !== $status && $status !== $task[AttributeInterface::ATTRIBUTE_STATUS]) {
                         continue;
                     }
 
-                    // Filter props if exists
-                    if ($this->props) {
-                        $task = ArrayUtils::only($task, $this->props);
+                    if (AttributeInterface::ATTRIBUTE_STATUS == $attribute) {
+                        if (!isset($annotationTask[$attribute])) {
+                            $task[$attribute] = StatusInterface::STATUS_ACTIVE;
+                        } else {
+                            $task[$attribute] = $annotationTask[$attribute];
+                        }
+
+                        continue;
                     }
 
-                    $tasks[] = $task;
+                    if (AttributeInterface::ATTRIBUTE_RESOURCE == $attribute) {
+                        $task[$attribute] = ResourceInterface::RESOURCE_ANNOTATION;
+
+                        continue;
+                    }
+
+                    $task[$attribute] = $annotationTask[$attribute] ?? null;
                 }
+
+                // Filter by status
+                if (null !== $status && $status !== $task[AttributeInterface::ATTRIBUTE_STATUS]) {
+                    continue;
+                }
+
+                // Filter props if exists
+                if ($this->props) {
+                    $task = ArrayUtils::only($task, $this->props);
+                }
+
+                $tasks[] = $task;
             }
         }
 
